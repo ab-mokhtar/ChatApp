23b2ad7b8c6aeb1054ce385b2d1f930a7514af80
ab-mokhtar <mokhtarabbes9@gmail.com>
2024-01-11 10:40:17 +0100

    deploy project

---
+++ src/app/Components/discussion/discussion.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+import Peer from 'peerjs';
+
+@Component({
+  selector: 'app-discussion',
+  templateUrl: './discussion.component.html',
+  styleUrls: ['./discussion.component.css']
+})
+export class DiscussionComponent implements OnInit {
+  private peer: Peer;
+  peerIdShare!: string;
+  peerId!: string;
+  private lazyStream: any;
+  currentPeer: any;
+  private peerList: Array<any> = [];
+
+  constructor() {
+    this.peer = new Peer({
+      //ip adress of aws machine
+      host: '3.89.85.44',
+      port: 9000, // Update with the port your PeerJS server is running on
+      path: '/myapp', // Update with the path you configured
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPeerId();
+  }
+
+  private getPeerId = () => {
+    this.peer.on('open', (id) => {
+      this.peerId = id;
+    });
+
+    this.peer.on('call', (call) => {
+      navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      }).then((stream) => {
+        this.lazyStream = stream;
+
+        call.answer(stream);
+        call.on('stream', (remoteStream) => {
+          if (!this.peerList.includes(call.peer)) {
+            this.streamRemoteVideo(remoteStream);
+            this.currentPeer = call.peerConnection;
+            this.peerList.push(call.peer);
+          }
+        });
+      }).catch(err => {
+        console.log(err + 'Unable to get media');
+      });
+    });
+  }
+
+  connectWithPeer(): void {
+    this.callPeer(this.peerIdShare);
+  }
+
+  private callPeer(id: string): void {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then((stream) => {
+      this.lazyStream = stream;
+
+      const call = this.peer.call(id, stream);
+      call.on('stream', (remoteStream) => {
+        if (!this.peerList.includes(call.peer)) {
+          this.streamRemoteVideo(remoteStream);
+          this.currentPeer = call.peerConnection;
+          this.peerList.push(call.peer);
+        }
+      });
+    }).catch(err => {
+      console.log(err + 'Unable to connect');
+    });
+  }
+
+  private streamRemoteVideo(stream: any): void {
+    const video = document.createElement('video');
+    video.classList.add('video');
+    video.srcObject = stream;
+    video.play();
+
+    const remoteVideoContainer = document.getElementById('remote-video');
+
+if (remoteVideoContainer !== null) {
+    // Create or obtain your 'video' element
+
+    // Append the 'video' element to the 'remote-video' container
+    remoteVideoContainer.append(video);
+} else {
+    console.error("Remote video container not found.");
+}
+
+  }
+
+  screenShare(): void {
+    this.shareScreen();
+  }
+
+  private shareScreen(): void {
+    // @ts-ignore
+    navigator.mediaDevices.getDisplayMedia({
+      video: {
+        cursor: 'always' 
+      }as MediaTrackConstraints,
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true
+      }
+    }).then(stream => {
+      const videoTrack = stream.getVideoTracks()[0];
+      videoTrack.onended = () => {
+        this.stopScreenShare();
+      };
+
+      const sender = this.currentPeer.getSenders().find((s: RTCRtpSender)=> s.track?.kind === videoTrack.kind);
+      sender.replaceTrack(videoTrack);
+    }).catch(err => {
+      console.log('Unable to get display media ' + err);
+    });
+  }
+
+  private stopScreenShare(): void {
+    const videoTrack = this.lazyStream.getVideoTracks()[0];
+    const sender = this.currentPeer.getSenders().find((s: RTCRtpSender)=> s.track?.kind === videoTrack.kind);
+    sender.replaceTrack(videoTrack);
+  }
+
+}
